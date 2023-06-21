0cf0a6665790f5e031b87595a5e3e4a6faaa171f
Mizumoto <mizumotokunn@gmail.com>
2023-06-21 18:04:56 +0900

    command&option CODE

---
+++ pkg/protocol/command.go
@@ -0,0 +1,130 @@
+package protocol
+
+const (
+	// 无操作 No operation
+	// IPMSG_NOOPERATION       0x00000000
+	IPMSG_NOOPERATION uint64 = 0x00000000
+
+	// 开启服务(启动时广播) Start service (broadcast at startup)
+	// IPMSG_BR_ENTRY          0x00000001
+	IPMSG_BR_ENTRY uint64 = 0x00000001
+
+	// 关闭服务(结束时广播) Stop service (broadcast at end)
+	// IPMSG_BR_EXIT           0x00000002
+	IPMSG_BR_EXIT uint64 = 0x00000002
+
+	// 在线通知 Online notification
+	// IPMSG_ANSENTRY          0x00000003
+	IPMSG_ANSENTRY uint64 = 0x00000003
+
+	// 更改为离线模式 Change to offline mode
+	// IPMSG_BR_ABSENCE        0x00000004
+	IPMSG_BR_ABSENCE uint64 = 0x00000004
+
+	///////////////////////////////////////////
+	// 搜索可通信的用户列表 Search for a list of users who can communicate
+	// IPMSG_BR_ISGETLIST      0x00000010
+	IPMSG_BR_ISGETLIST uint64 = 0x00000010
+
+	// 上面命令的确认命名，把自己标记为可通信 Confirm the above command and mark yourself as communicable
+	// IPMSG_OKGETLIST         0x00000011
+	IPMSG_OKGETLIST uint64 = 0x00000011
+
+	// 用户列表请求 User list request
+	// IPMSG_GETLIST           0x00000012
+	IPMSG_GETLIST uint64 = 0x00000012
+
+	// 用户列表传输 User list transfer
+	// IPMSG_ANSLIST           0x00000013
+	IPMSG_ANSLIST uint64 = 0x00000013
+
+	///////////////////////////////////////////
+	// 发送消息 Send message
+	// IPMSG_SENDMSG           0x00000020
+	IPMSG_SENDMSG uint64 = 0x00000020
+
+	// 确认消息接收 Confirm message reception
+	// IPMSG_RECVMSG           0x00000021
+	IPMSG_RECVMSG uint64 = 0x00000021
+
+	///////////////////////////////////////////
+	// 加密消息阅读通知 Encrypted message read notification
+	// IPMSG_READMSG           0x00000030
+	IPMSG_READMSG uint64 = 0x00000030
+
+	// 加密消息废弃通知 Encrypted message discard notification
+	// IPMSG_DELMSG            0x00000031
+	IPMSG_DELMSG uint64 = 0x00000031
+
+	// 加密消息阅读确认(版本8追加) Encrypted message read confirmation (added in version 8)
+	// IPMSG_ANSREADMSG        0x00000032
+	IPMSG_ANSREADMSG uint64 = 0x00000032
+
+	///////////////////////////////////////////
+	// 附件请求 Attachment request
+	// IPMSG_GETFILEDATA       0x00000060
+	IPMSG_GETFILEDATA uint64 = 0x00000060
+
+	// 附件请求放弃 Attachment request abandoned
+	// IPMSG_RELEASEFILES      0x00000061
+	IPMSG_RELEASEFILES uint64 = 0x00000061
+
+	// 文件夹附件请求 Folder attachment request
+	// IPMSG_GETDIRFILES       0x00000062
+	IPMSG_GETDIRFILES uint64 = 0x00000062
+
+	///////////////////////////////////////////
+	// 获取 IPMSG 版本 Get IPMSG version
+	// IPMSG_GETINFO           0x00000040
+	IPMSG_GETINFO uint64 = 0x00000040
+
+	// 发送 IPMSG 版本
+	// IPMSG_SENDINFO          0x00000041
+	IPMSG_SENDINFO uint64 = 0x00000041
+
+	///////////////////////////////////////////
+	// 获取离线信息 Get offline information
+	// IPMSG_GETABSENCEINFO    0x00000050
+	IPMSG_GETABSENCEINFO uint64 = 0x00000050
+
+	// 发送离线信息
+	// IPMSG_SENDABSENCEINFO   0x00000051
+	IPMSG_SENDABSENCEINFO uint64 = 0x00000051
+
+	///////////////////////////////////////////
+	// 获取 RSA 公匙 RSA public key acquisition
+	// IPMSG_GETPUBKEY         0x00000072
+	IPMSG_GETPUBKEY uint64 = 0x00000072
+
+	// RSA 公匙响应
+	// IPMSG_ANSPUBKEY         0x00000073
+	IPMSG_ANSPUBKEY uint64 = 0x00000073
+
+	///////////////////////////////////////////
+	// 群成员在线通知群主 Group member online notification group owner
+	// IPMSG_DIR_POLL          0x000000b0
+	IPMSG_DIR_POLL uint64 = 0x000000b0
+
+	// 群管理员任命 Agent appointment
+	// IPMSG_DIR_POLLAGENT     0x000000b1
+	IPMSG_DIR_POLLAGENT uint64 = 0x000000b1
+
+	// 群主任命管理员广播请求 Group owner appoints administrator broadcast request
+	// IPMSG_DIR_BROADCAST     0x000000b2
+	IPMSG_DIR_BROADCAST uint64 = 0x000000b2
+
+	// 群主任命管理员广播响应 Group owner appoints administrator broadcast response
+	// IPMSG_DIR_ANSBROAD      0x000000b3
+	IPMSG_DIR_ANSBROAD uint64 = 0x000000b3
+
+	// 群主更改群成员名单通知 Group owner changes group member list notification
+	// IPMSG_DIR_PACKET        0x000000b4
+	IPMSG_DIR_PACKET uint64 = 0x000000b4
+
+	// 群主解除管理员请求 Group owner relieves administrator request
+	// IPMSG_DIR_REQUEST       0x000000b5
+	IPMSG_DIR_REQUEST uint64 = 0x000000b5
+	// 管理员解除自我管理 Group owner relieves self management
+	// IPMSG_DIR_AGENTPACKET   0x000000b6
+	IPMSG_DIR_AGENTPACKET uint64 = 0x000000b6
+)
---
+++ pkg/protocol/option.go
@@ -0,0 +1,52 @@
+package protocol
+
+const (
+	// 离线模式(与成员识别命令一起使用)
+	IPMSG_ABSENCEOPT uint64 = 0x00000100
+	// 服务器(预订)
+	IPMSG_SERVEROPT uint64 = 0x00000200
+	// 成员识别命令
+	IPMSG_DIALUPOPT uint64 = 0x00010000
+
+	// 检查发送
+	IPMSG_SENDCHECKOPT uint64 = 0x00000100
+	// 加密
+	IPMSG_SECRETOPT uint64 = 0x00000200
+	// 检查加密(版本8追加)
+	IPMSG_PASSWORDOPT uint64 = 0x00008000
+	// 广播(全局选择)
+	IPMSG_BROADCASTOPT uint64 = 0x00000400
+	// 多播(多个选择)
+	IPMSG_MULTICASTOPT uint64 = 0x00000800
+	// 新版本多播
+	IPMSG_NEWMULTIOPT uint64 = 0x00040000
+	// 不写入日志
+	IPMSG_NOLOGOPT uint64 = 0x00020000
+	// 启启动时不加入用户列表
+	IPMSG_NOADDLISTOPT uint64 = 0x00080000
+	// 自动应答
+	IPMSG_AUTORETOPT uint64 = 0x00002000
+
+	// 附件
+	IPMSG_FILEATTACHOPT uint64 = 0x00200000
+	// 密码
+	IPMSG_ENCRYPTOPT uint64 = 0x00400000
+	// 在密文中包含附件信息和目的地信息
+	IPMSG_ENCEXTMSGOPT uint64 = 0x04000000
+
+	// 使用UTF-8
+	IPMSG_CAPUTF8OPT uint64 = 0x01000000
+	// 消息使用 UTF-8
+	IPMSG_UTF8OPT uint64 = 0x00800000
+	// 支持嵌入图片的消息
+	IPMSG_CLIPBOARDOPT uint64 = 0x08000000
+	// 附件加密请求
+	IPMSG_ENCFILEOPT uint64 = 0x00000800
+	// 支持 IPDict 格式
+	IPMSG_CAPIPDICTOPT uint64 = 0x02000000
+	// 群主
+	IPMSG_DIR_MASTER uint64 = 0x10000000
+
+	// 重传标志(HOSTLIST 获取时使用)
+	IPMSG_RETRYOPT uint64 = 0x00004000
+)
